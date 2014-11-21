81ccde7c51975f7265cf170582ca5b3e3d2206fc
hugoabonizio <hugo_abonizio@hotmail.com>
2014-11-21 15:29:07 +0000

    delete garbage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def bs_icon(name)
+    raw "<i class='glyphicon glyphicon-#{name}'></i>"
+  end
 end
--- app/views/projects/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @project, :id, :name, :user_id, :created_at, :updated_at
